0e3e290ca600ca618b51476926882eeb8303b5c0
Emanuel Hasbun <ehasbung@gmail.com>
2021-07-18 20:02:44 -0600

    primer commit marchamo

---
+++ AppIBULACIT/Views/frmMarchamo.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Async="true" Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="frmMarchamo.aspx.cs" Inherits="AppIBULACIT.Views.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
--- WebApiSegura/Controllers/LoginController.cs
+++ WebApiSegura/Controllers/LoginController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApiSegura.Models;
+
 namespace WebApiSegura.Controllers
 {
     [AllowAnonymous]
@@ -18,6 +19,7 @@ namespace WebApiSegura.Controllers
         [Route("authenticate")]
         public IHttpActionResult Authenticate(LoginRequest loginRequest)
         {
+
             if (loginRequest == null)
                 return BadRequest();
 
@@ -25,32 +27,30 @@ namespace WebApiSegura.Controllers
 
             try
             {
-                using (SqlConnection sqlConnection = 
+                using (SqlConnection sqlConnection =
                     new SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
                 {
-                    SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo, Identificacion, Nombre,
-                                                             Username, Password, Email, FechaNacimiento, Estado
-                                                            FROM Usuario Where Username = @Username and Password = @Password ", sqlConnection);
-                    
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT Codigo, Identificacion, Nombre, 
+                                                           Username, Password, Email, FechaNacimiento, Estado
+                                                           From Usuario WHERE Username = @Username and Password = @Password", sqlConnection);
+
                     sqlCommand.Parameters.AddWithValue("@Username", loginRequest.Username);
                     sqlCommand.Parameters.AddWithValue("@Password", loginRequest.Password);
 
                     sqlConnection.Open();
-
                     SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
 
                     while (sqlDataReader.Read())
                     {
                         usuario.Codigo = sqlDataReader.GetInt32(0);
                         usuario.Identificacion = sqlDataReader.GetString(1);
-                        usuario.Nombre = sqlDataReader.GetString(2);
                         usuario.Username = sqlDataReader.GetString(3);
                         usuario.Password = sqlDataReader.GetString(4);
                         usuario.Email = sqlDataReader.GetString(5);
                         usuario.FechaNacimiento = sqlDataReader.GetDateTime(6);
                         usuario.Estado = sqlDataReader.GetString(7);
 
-                        var token = 
+                        var token =
                             TokenGenerator.GenerateTokenJwt(usuario.Identificacion);
                         usuario.Token = token;
                     }
@@ -58,17 +58,22 @@ namespace WebApiSegura.Controllers
                     sqlConnection.Close();
 
                     if (!string.IsNullOrEmpty(usuario.Token))
+                    {
                         return Ok(usuario);
+                    }
                     else
+                    {
                         return Unauthorized();
-                } 
+                    }
+
+                }
             }
             catch (Exception ex)
             {
                 return InternalServerError(ex);
             }
-
         }
+
 
         [HttpPost]
         [Route("register")]
@@ -76,18 +81,16 @@ namespace WebApiSegura.Controllers
         {
             if (usuario == null)
                 return BadRequest();
-
             try
             {
-                using(SqlConnection sqlConnection = new 
-                    SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
+                using (SqlConnection sqlConnection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["INTERNET_BANKING"].ConnectionString))
                 {
-                    SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO Usuario (Identificacion, Nombre, Username, 
+                    SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO Usuario(Identificacion, Nombre, Username, 
                                                             Password, Email, FechaNacimiento, Estado) VALUES 
-                                                            (@Identificacion, @Nombre, @Username, 
-                                                            @Password, @Email, @FechaNacimiento, @Estado) ", sqlConnection);
+                                                            (@Identificacion, @Nombre, @Username, @Password, @Email, @FechaNacimiento, @Estado)", sqlConnection);
 
-                    sqlCommand.Parameters.AddWithValue("@Identificacion",usuario.Identificacion);
+                    sqlCommand.Parameters.AddWithValue("@Identificacion", usuario.Identificacion);
                     sqlCommand.Parameters.AddWithValue("@Nombre", usuario.Nombre);
                     sqlCommand.Parameters.AddWithValue("@Username", usuario.Username);
                     sqlCommand.Parameters.AddWithValue("@Password", usuario.Password);
@@ -100,6 +103,7 @@ namespace WebApiSegura.Controllers
                     int filasAfectadas = sqlCommand.ExecuteNonQuery();
 
                     sqlConnection.Close();
+
                 }
             }
             catch (Exception ex)
