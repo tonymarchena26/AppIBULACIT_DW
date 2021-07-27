0d1d14e8f62a32ca542dc52236df13cc703d61cd
cgonzalezh333 <claudiogh33@gmail.com>
2021-07-27 17:48:01 -0600

    managers infraccion-pruebas

---
+++ AppIBULACIT/Models/Infracciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppIBULACIT.Models
+{
+    public class Infracciones
+    {
+        public int Codigo { get; set; }
+        public int Cedula { get; set; }
+        public string TipoInfraccion { get; set; }
+        public int Monto { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
